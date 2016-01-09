991d1e3f84152ef6ab49e55eeae2c1bbb8f51ee5
bilal islam <bilal-islam@hotmail.com>
2016-01-09 22:59:48 +0200

    test

--- WcfServiceCommon/WcfServiceClient.cs
+++ WcfServiceCommon/WcfServiceClient.cs
@@ -7,49 +7,49 @@ using System.ServiceModel.Channels;
 
 namespace WcfServiceCommon
 {
-	public class WcfServiceClient<TClient> : ClientBase<TClient>, IDisposable where TClient : class
-	{
-		public static WcfServiceClient<TClient> Create(string key)
-		{
-			Type clientType = typeof(TClient);
-			var configItem = Config.GetServiceConfig(clientType);
-			if (null != configItem)
-			{
-				NetTcpBinding tcpBinding = TcpBindingUtility.CreateNetTcpBinding();
-				EndpointAddress endpointAddress = TcpBindingUtility.CreateEndpointAddress(
-					configItem.Item.ServiceAddressPort + "/" + configItem.Item.EndpointName);
+    public class WcfServiceClient<TClient> : ClientBase<TClient>, IDisposable where TClient : class
+    {
+        public static WcfServiceClient<TClient> Create(string key)
+        {
+            Type clientType = typeof(TClient);
+            var configItem = Config.GetServiceConfig(clientType);
+            if (null != configItem)
+            {
+                NetTcpBinding tcpBinding = TcpBindingUtility.CreateNetTcpBinding();
+                EndpointAddress endpointAddress = TcpBindingUtility.CreateEndpointAddress(
+                    configItem.Item.ServiceAddressPort + "/" + configItem.Item.EndpointName);
 
-				WcfServiceClient<TClient> client = new WcfServiceClient<TClient>(tcpBinding, endpointAddress);
-				return client;
-			}
-			throw new Exception("Type " + clientType.AssemblyQualifiedName + " not found.");
-		}
+                WcfServiceClient<TClient> client = new WcfServiceClient<TClient>(tcpBinding, endpointAddress);
+                return client;
+            }
+            throw new Exception(clientType.AssemblyQualifiedName);
+        }
 
-		internal WcfServiceClient() { }
+        internal WcfServiceClient() { }
 
-		internal WcfServiceClient(string endpointConfigurationName) : base(endpointConfigurationName) { }
+        internal WcfServiceClient(string endpointConfigurationName) : base(endpointConfigurationName) { }
 
-		internal WcfServiceClient(Binding binding, EndpointAddress remoteAddress) : base(binding, remoteAddress) { }
+        internal WcfServiceClient(Binding binding, EndpointAddress remoteAddress) : base(binding, remoteAddress) { }
 
-		internal WcfServiceClient(InstanceContext callbackInstance) : base(callbackInstance) { }
+        internal WcfServiceClient(InstanceContext callbackInstance) : base(callbackInstance) { }
 
-		public TClient Instance
-		{
-			get { return base.Channel; }
-		}
+        public TClient Instance
+        {
+            get { return base.Channel; }
+        }
 
-		public void Dispose()
-		{
-			AbortClose();
-		}
+        public void Dispose()
+        {
+            AbortClose();
+        }
 
-		public void AbortClose()
-		{
-			//avoid the CommunicationObjectFaultedException 
-			if (this.State != CommunicationState.Closed) this.Abort();
-			//safe to close the client
-			this.Close();
-		}
+        public void AbortClose()
+        {
+            //avoid the CommunicationObjectFaultedException 
+            if (this.State != CommunicationState.Closed) this.Abort();
+            //safe to close the client
+            this.Close();
+        }
 
-	}
+    }
 }
